3877a197d75660a6415f66c146fd0a04860a62a7
Mark Villudo <markanthony.villudo@gmail.com>
2018-12-04 14:48:32 +0800

    Updates edit user layout

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Dashboard</title>
+  <title>HRIS 2 | Dashboard</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -45,9 +45,9 @@
     <!-- Logo -->
     <a href="index2.html" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>A</b>LT</span>
+      <span class="logo-mini"><b>HRIS</b></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Admin</b>LTE</span>
+      <span class="logo-lg"><b>HRIS</b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
@@ -72,7 +72,7 @@
                   <li><!-- start message -->
                     <a href="#">
                       <div class="pull-left">
-                        <img src="{{asset('/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
+                        <img src="{{asset('../dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         Support Team
@@ -85,7 +85,7 @@
                   <li>
                     <a href="#">
                       <div class="pull-left">
-                        <img src="{{asset('dist/img/user3-128x128.jpg')}}" class="img-circle" alt="User Image">
+                        <img src="{{asset('../dist/img/user3-128x128.jpg')}}" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         AdminLTE Design Team
@@ -97,7 +97,7 @@
                   <li>
                     <a href="#">
                       <div class="pull-left">
-                        <img src="{{asset('dist/img/user4-128x128.jpg')}}" class="img-circle" alt="User Image">
+                        <img src="{{asset('../dist/img/user4-128x128.jpg')}}" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         Developers
@@ -109,7 +109,7 @@
                   <li>
                     <a href="#">
                       <div class="pull-left">
-                        <img src="{{asset('dist/img/user3-128x128.jpg')}}" class="img-circle" alt="User Image">
+                        <img src="{{asset('../dist/img/user3-128x128.jpg')}}" class="img-circle" alt="User Image">
                       </div>
                       <h4>
                         Sales Department
@@ -258,14 +258,13 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="{{asset('../dist/img/user2-160x160.jpg')}}" class="user-image" alt="User Image">
+              <img src="{{asset('../dist/img/user2-160x160.jpg')}}" class="user-image" alt="User Image2">
               <span class="hidden-xs">Alexander Pierce</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="{{asset('../dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
-
+                <img src="{{asset('../dist/img/user2-160x160.jpg')}}" class="user-image" alt="User Image1">
                 <p>
                   Alexander Pierce - Web Developer
                   <small>Member since Nov. 2012</small>
