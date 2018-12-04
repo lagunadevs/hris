d44756e5ae5f3eb16747fc4a5d37b3d253a41e27
Mark Villudo <markanthony.villudo@gmail.com>
2018-12-04 13:46:12 +0800

    Admin Lte template package and publish assets plus views content

--- config/app.php
+++ config/app.php
@@ -176,6 +176,8 @@ return [
         App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        MarkVilludo\AdminLte\ServiceProvider::class,
+
     ],
 
     /*
