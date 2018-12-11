d1944e97a0ebd0ab667fd71aa83c4f0d52c4b90e
paulkatipunan <paul.katipunan@synergy88digital.com>
2018-12-11 17:18:31 +0800

    update

--- resources/js/pages/App.vue
+++ resources/js/pages/App.vue
@@ -1,17 +1,11 @@
 <template>
+    
     <div class="content">
-        <h1>Vue Router Demo App</h1>
 
-        <p>
-            <router-link :to="{ name: 'about' }">about</router-link> |
-            <router-link :to="{ name: 'home' }">Hello World</router-link>
-            
-        </p>
-
-        
-            <router-view></router-view>
+        <router-view></router-view>
       
     </div>
+
 </template>
 
 <script>
