0d01682c35a74f3d16f8a4473d5ea8f122f96bf0
markvilludo <markanthony.villudo@gmail.com>
2018-12-27 16:26:38 -0800

    fix, key too long error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use App;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,5 +28,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        
+        
+        Schema::defaultStringLength(191);
     }
 }
